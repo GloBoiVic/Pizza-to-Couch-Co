750af0f50df55e074b9ed2428d74b6aa650821d7
GloBoiVic <victorike08@gmail.com>
2023-10-24 11:18:52 -0500

    Add user's position to order

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,22 +2,33 @@ import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant.js';
 import Button from '../../ui/Button.jsx';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice.js';
 import EmptyCart from '../cart/EmptyCart.jsx';
 import store from '../../store.js';
 import { formatCurrency } from '../../utils/helpers.js';
+import { fetchAddress } from '../user/userSlice.js';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) => /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str);
 
 function CreateOrder() {
   const [withPriority, setWithPriority] = useState(false);
-  const username = useSelector((state) => state.user.username);
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    error: errorAddress,
+  } = useSelector((state) => state.user);
+  const isLoadingAddress = address === 'loading';
+
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
 
   const formErrors = useActionData();
+  const dispatch = useDispatch();
+
   const cart = useSelector(getCart);
   const totalCartPrice = useSelector(getTotalCartPrice);
   const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
@@ -46,11 +57,36 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="flex flex-col gap-2 mb-5 sm:flex-row sm:items-center">
+        <div className="relative flex flex-col gap-2 mb-5 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
-            <input type="text" name="address" required className="w-full input" />
+            <input
+              type="text"
+              name="address"
+              required
+              className="w-full input"
+              disabled={isLoadingAddress}
+              defaultValue={address}
+            />
+            {addressStatus === 'error' && (
+              <p className="p-2 mt-2 text-xs text-red-700 bg-red-100 rounded-md ">{errorAddress}</p>
+            )}
           </div>
+
+          {!position.latitude && !position.longitude && (
+            <span className="absolute right-[3px] top-[35px] sm:top-[5px] sm:right-[5px] z-50">
+              <Button
+                variant={'small'}
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}
+                disabled={isLoadingAddress}
+              >
+                Get position
+              </Button>
+            </span>
+          )}
         </div>
 
         <div className="flex items-center gap-5 mb-12">
@@ -69,7 +105,12 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button variant="primary" disabled={isSubmitting}>
+          <input
+            type="hidden"
+            name="position"
+            value={position.longitude && position.latitude ? `${position.latitude},${position.longitude}` : ''}
+          />
+          <Button variant="primary" disabled={isSubmitting || isLoadingAddress}>
             {isSubmitting ? 'Placing order...' : `Order now from ${formatCurrency(totalPrice)}`}
           </Button>
         </div>
@@ -84,12 +125,15 @@ export const action = async ({ request }) => {
   const data = Object.fromEntries(formData);
 
   const order = { ...data, cart: JSON.parse(data.cart), priority: data.priority === 'true' };
+  console.log(order);
 
   const errors = {};
 
   if (!isValidPhone(order.phone)) errors.phone = 'Please provide a valid phone number so we can contact you';
 
   if (Object.keys(errors).length > 0) return errors;
+
+  // If everything is okay, create new order and redirect
 
   const newOrder = await createOrder(order);
 
